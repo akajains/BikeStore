66b470a1e4d9643efb1fa6acbbb76cf70ffa1bb2
Aakash <aakash.jain@itemgroup.com.au>
2020-01-29 23:17:33 +1100

    Fixed version mismatch between entityframework Rename project to BikeStore.Web

--- BikeStore.Data/Models/BikeStoresContext.cs
+++ BikeStore.Data/Models/BikeStoresContext.cs
@@ -29,7 +29,6 @@ namespace BikeStore.Data.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Server=(local);Database=BikeStores;Trusted_Connection=True;");
             }
         }
